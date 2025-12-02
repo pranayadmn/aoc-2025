9ad56ef6bc321e2334d77dbe4e8a3e9e9208bb67
Pranaya Deomani <mail@pranaya.dev>
2025-12-03 02:08:54 +0530

    Day 1: Secret Entrance

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,51 @@
 fun main() {
+    // fun part1(input: List<String>): Int {
+    //     var dial = 50
+    //     var zeroes = 0
+    //     input.forEach {
+    //         if (it[0] == 'L') {
+    //             dial = (dial - it.drop(1).toInt()) % 100
+    //         } else {
+    //             dial = (dial + it.drop(1).toInt()) % 100
+    //         }
+    //         if (dial == 0) zeroes++
+    //     }
+    //     return zeroes
+    // }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        return input.count {
+            dial = if (it[0] == 'L') {
+                (dial - it.drop(1).toInt()) % 100
+            } else {
+                (dial + it.drop(1).toInt()) % 100
+            }
+            dial == 0
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var zeroes = 0
+        input.forEach {
+            val delta = if (it[0] == 'L') -1 else 1
+
+            repeat(it.drop(1).toInt()) {
+                dial = (dial + delta) % 100
+                if (dial == 0) zeroes++
+            }
+        }
+        return zeroes
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
